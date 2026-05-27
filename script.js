61ac4a95b6dee5149d2a5e1481547538b6cb56fd
EnzoBarcelos123 <enzo.barcelos@escola.pr.gov.br>
2026-05-27 20:39:40 -0300

    teste

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+// Scroll suave para navegação
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
